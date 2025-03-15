a4da5bfed1d1b23b093194fd2d1453acdbb9afa3
Spam <mscaramanti@gmail.com>
2025-03-15 19:24:51 -0400

    Base code made, giving random jokes

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const port = 4000;
 
 app.use(express.static("public"));
 app.use(urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs")
+    try {
+        const response = await axios.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single");
+        const result = response.data;
+        console.log(result.joke);  
+        res.render("index", { jokes: result });  
+    } catch (error) {
+        console.error("Error", error.message);
+        res.render("index", { error: error.message });
+    }
 });
-
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
