63b9bd3a5cf56bf19d3f048c4c6077710c253ab3
Spam <mscaramanti@gmail.com>
2025-03-16 16:04:58 -0400

    Search box added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import axios from "axios"
 
 const app = express();
 const port = 4000;
+const APIsearch = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single&contains=";
+const APIrandom = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single";
 
 app.use(express.static("public"));
 app.use(urlencoded({ extended: true }));
@@ -10,13 +12,24 @@ app.set('view engine', 'ejs');
 
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single");
+        const searchTerm = req.query.searchTerm;
+        let response;
+
+        if (searchTerm) {
+            const encodedTerm = encodeURIComponent(searchTerm.trim());
+            response = await axios.get(APIsearch + encodedTerm);
+        } else {
+            response = await axios.get(APIrandom);
+        }
+
         const result = response.data;
-        console.log(result.joke);  
-        res.render("index", { jokes: result });  
+        console.log(result.joke);
+
+        res.render("index", { jokes: result, search: searchTerm });
+
     } catch (error) {
-        console.error("Error", error.message);
-        res.render("index", { error: error.message });
+        console.error("Error:", error.message);
+        res.render("index", { error: "Sorry, no jokes found!" });
     }
 });
 
